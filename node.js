f1d5bf6753d72dfa1fbb2f1c87870ae3dffaa427
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-04 10:25:48 +0545

    first commit

---
+++ node.js
@@ -0,0 +1,16 @@
+console.log("enter the name of your friend");
+const friends = ["nirmal","dhakal","arun","neupane","hello",'nabin']
+console.log(friends[0],friends[1])
+
+
+//  properties and method
+
+// console.log(shoes.length) property
+// console.log(shoes.length()) method
+friends.unshift("rachana") 
+// suru ma push garni
+friends.push("prajwol")
+// lat ma push garna paryo vani 
+friends[3] = "rampur"
+console.log(friends.length)
+console.log(friends)
