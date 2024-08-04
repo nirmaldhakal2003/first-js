3ce86e60f59e04cb39ddb9d0aec22d95265d4919
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-04 19:10:29 +0545

    casing  concept

---
+++ casing.js
@@ -0,0 +1,20 @@
+// Camel Case
+//  = camel case le first letter of word lie chai capatilize garxa 
+
+const FirstName
+const ILoveProgramming
+
+
+// snake_case
+// firstname -->first_name
+// word ko bich ma underscore rakhxa snake_case le
+
+
+// kebab case
+// firstname --> kebab-case --> first-name 
+
+
+// pascleCase
+// sabai word lie capatilize garxa 
+// ilovejs --> ILOVEJS
+
